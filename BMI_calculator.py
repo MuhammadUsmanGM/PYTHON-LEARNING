6d73e2706d29d7bd05ef095505deda22abe67094
Muhammad Usman GM <muhammadusman75425965etc@gamil.com>
2025-03-17 14:26:46 +0500

    changes theme of bmi.py

--- BMI_calculator.py
+++ BMI_calculator.py
@@ -1,6 +1,22 @@
 import streamlit as st
 import pandas as pd
-print('Welcome to BMI Calculator')
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background: linear-gradient(to right,rgb(2, 30, 109),rgb(99, 5, 5),rgb(122, 108, 0),rgb(26, 131, 7), #005F69);
+    }
+    
+    .css-1d391kg { /* Sidebar background */
+        background: linear-gradient(to bottom,rgb(6, 74, 109),rgb(4, 106, 124));
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+st.write('Welcome to BMI Calculator')
 height=st.slider('Enter your height(in cm): ',20,250,175)
 height=float(height)
 weight=st.slider('Enter your weight(in kg): ',20,200,70)
